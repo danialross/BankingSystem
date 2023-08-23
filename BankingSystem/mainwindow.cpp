d753a98cf1fff428729db1af1e0130b05a1efca8
danialross <92746365+danialross@users.noreply.github.com>
2023-08-23 16:54:14 -0400

    changed ui

--- BankingSystem/mainwindow.cpp
+++ BankingSystem/mainwindow.cpp
@@ -8,9 +8,6 @@ MainWindow::MainWindow(QWidget *parent)
 {
 
     bank = new Bank(4);
-    for (const auto& pair : bank->getCards()) {
-        std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
-    }
     ui->setupUi(this);
 }
 
@@ -24,6 +21,7 @@ void MainWindow::on_pushButton_1_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "1");
+    ui->enterButton->setEnabled(true);
 }
 
 
@@ -31,6 +29,7 @@ void MainWindow::on_pushButton_2_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "2");
+    ui->enterButton->setEnabled(true);
 }
 
 
@@ -39,54 +38,63 @@ void MainWindow::on_pushButton_3_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "3");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_4_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "4");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_5_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "5");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_6_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "6");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_7_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "7");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_8_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "8");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_9_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "9");
+    ui->enterButton->setEnabled(true);
 }
 
 void MainWindow::on_pushButton_0_clicked()
 {
     QString currText = ui->lineEdit->text();
     ui->lineEdit->setText(currText + "0");
+    ui->enterButton->setEnabled(true);
 }
 
 
 void MainWindow::on_clearButton_clicked()
 {
     ui->lineEdit->setText("");
+    ui->enterButton->setEnabled(false);
 }
 
 void MainWindow::on_enterButton_clicked()
@@ -99,11 +107,11 @@ void MainWindow::on_enterButton_clicked()
 
     cout << it->first << endl;
 
-    /*if(it != bank.getCards().end()){
+    if(it != bank->getCards().end()){
         // change to pin
     }else{
         ui->lineEdit->setText("");
         ui->errorLabel->setText("Invalid Account Number");
-    }*/
+    }
 
 }
